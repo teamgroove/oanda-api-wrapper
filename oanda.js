306919ddba5239f18738a5438923eadb1c7d7077
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2020-01-01 13:07:41 -0500

    Implement getAccountDetails() and simplify strings

--- oanda.js
+++ oanda.js
@@ -2,7 +2,7 @@ const CONFIG = require('./config');
 
 const axios = require('axios');
 
-const BASE_URL = 'https://api-fxpractice.oanda.com';
+const BASE_URL = `https://api-fxpractice.oanda.com/v3/accounts/${CONFIG.accountID}`;
 const HEADER = {
     headers: { // this is done because axios requires that the last argument be an object containing 'headers'
         'Content-Type': 'application/json',
@@ -10,8 +10,17 @@ const HEADER = {
     }
 };
 
+async function getAccountDetails() {
+    try {
+        const response = await axios.get(BASE_URL, HEADER);
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 async function getAccountSummary() {
-    const URL = `${BASE_URL}/v3/accounts/${CONFIG.accountID}/summary`;
+    const URL = `${BASE_URL}/summary`;
     try {
         const response = await axios.get(URL, HEADER);
         console.log(response.data);
@@ -21,7 +30,7 @@ async function getAccountSummary() {
 }
 
 async function getAllInstrumentDetails() {
-    const URL = `${BASE_URL}/v3/accounts/${CONFIG.accountID}/instruments`;
+    const URL = `${BASE_URL}/instruments`;
     try {
         const response = await axios.get(URL, HEADER);
         console.log(response.data);
@@ -31,7 +40,7 @@ async function getAllInstrumentDetails() {
 }
 
 async function getInstrumentDetails(instrument) {
-    const URL = `${BASE_URL}/v3/accounts/${CONFIG.accountID}/instruments?instruments=${instrument}`;
+    const URL = `${BASE_URL}/instruments?instruments=${instrument}`;
     try {
         const response = await axios.get(URL, HEADER);
         console.log(response.data);
@@ -40,4 +49,4 @@ async function getInstrumentDetails(instrument) {
     }
 }
 
-getInstrumentDetails('USD_CAD');
+getAccountDetails();
