9cbadfe6e6fe72689b1f6960b00c5b92f9e4e7fc
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2020-01-02 01:07:57 -0500

    Add Airbnb style linter to enforce consistent code style

--- endpoints/account.js
+++ endpoints/account.js
@@ -1,108 +1,116 @@
-const CONFIG = require('../config');
-const BASE_URL = `https://api-fxpractice.oanda.com/v3/accounts/${CONFIG.accountID}`;
-const HEADER = {
-    headers: { // this is done because axios requires that the last argument be an object containing 'headers'
-        'Content-Type': 'application/json',
-        'Authorization': CONFIG.authorization
-    }
-};
-
 const axios = require('axios');
 
+const CONFIG = require('../config');
+
+const BASE_URL = `https://api-fxpractice.oanda.com/v3/accounts/${CONFIG.accountID}`;
+const HEADER = {
+  headers: { // axios requires that the last argument is an object containing 'headers'
+    'Content-Type': 'application/json',
+    Authorization: CONFIG.authorization,
+  },
+};
+
 async function getAccountDetails() {
-    try {
-        const response = await axios.get(BASE_URL, HEADER);
-        console.log(response.data);
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await axios.get(BASE_URL, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function getAccountSummary() {
-    const URL = `${BASE_URL}/summary`;
-    try {
-        const response = await axios.get(URL, HEADER);
-        console.log(response.data);
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
+  const URL = `${BASE_URL}/summary`;
+  try {
+    const response = await axios.get(URL, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function getAllInstrumentDetails() {
-    const URL = `${BASE_URL}/instruments`;
-    try {
-        const response = await axios.get(URL, HEADER);
-        console.log(response.data);
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
+  const URL = `${BASE_URL}/instruments`;
+  try {
+    const response = await axios.get(URL, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function getSpecificInstrumentDetails(instrument) {
-    const URL = `${BASE_URL}/instruments?instruments=${instrument}`;
-    try {
-        const response = await axios.get(URL, HEADER);
-        console.log(response.data);
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
+  const URL = `${BASE_URL}/instruments?instruments=${instrument}`;
+  try {
+    const response = await axios.get(URL, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function setMarginRate(rate) { // rate must be a string
-    const URL = `${BASE_URL}/configuration`;
-    try {
-        const response = await axios.patch(
-            URL,
-            {
-                'marginRate': rate,
-            },
-            HEADER
-        );
-        console.log(response.data)
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+  const URL = `${BASE_URL}/configuration`;
+  try {
+    const response = await axios.patch(
+      URL,
+      {
+        marginRate: rate,
+      },
+      HEADER,
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function changeAccountAlias(alias) {
-    const URL = `${BASE_URL}/configuration`;
-    try {
-        const response = await axios.patch(
-            URL,
-            {
-                'alias': alias,
-            },
-            HEADER
-        );
-        console.log(response.data)
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+  const URL = `${BASE_URL}/configuration`;
+  try {
+    const response = await axios.patch(
+      URL,
+      {
+        alias,
+      },
+      HEADER,
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function getAccountChanges(transactionID) {
-    const URL = `${BASE_URL}/changes?sinceTransactionID=${transactionID}`;
-    try {
-        const response = await axios.get(URL, HEADER);
-        console.log(response.data);
-        return response.data
-    } catch (error) {
-        console.log(error);
-    }
+  const URL = `${BASE_URL}/changes?sinceTransactionID=${transactionID}`;
+  try {
+    const response = await axios.get(URL, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 module.exports = {
-    getAccountDetails, 
-    getAccountSummary,
-    getAllInstrumentDetails,
-    getSpecificInstrumentDetails,
-    setMarginRate,
-    changeAccountAlias,
-    getAccountChanges
-}
+  getAccountDetails,
+  getAccountSummary,
+  getAllInstrumentDetails,
+  getSpecificInstrumentDetails,
+  setMarginRate,
+  changeAccountAlias,
+  getAccountChanges,
+};
--- endpoints/instrument.js
+++ endpoints/instrument.js
@@ -1,71 +1,75 @@
-const CONFIG = require('../config');
-const BASE_URL = 'https://api-fxpractice.oanda.com/v3/instruments';
-const HEADER = {
-    headers: { // this is done because axios requires that the last argument be an object containing 'headers'
-        'Content-Type': 'application/json',
-        'Authorization': CONFIG.authorization
-    }
-};
-
 const axios = require('axios');
 
-async function getCandlestickData(instrument, parameters = null) {
-    const URL = `${BASE_URL}/${instrument}/candles${convertObjectToQuery(parameters)}`;
-    try {
-        const response = await axios.get(URL, HEADER);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log(error);
+const CONFIG = require('../config');
+
+const BASE_URL = 'https://api-fxpractice.oanda.com/v3/instruments';
+const HEADER = {
+  headers: { // axios requires that the last argument is an object containing 'headers'
+    'Content-Type': 'application/json',
+    Authorization: CONFIG.authorization,
+  },
+};
+
+function convertObjectToQuery(object) {
+  const keys = Object.keys(object);
+  const values = Object.values(object);
+  const { length } = keys;
+
+  if (object != null) {
+    let query = '?';
+    for (let i = 0; i < length - 1; i += 1) {
+      query += `${keys[i]}=${values[i]}&`;
     }
+    query += `${keys[length - 1]}=${values[length - 1]}`;
+    return query;
+  }
+  return '';
+}
+
+async function getCandlestickData(instrument, parameters = null) {
+  const URL = `${BASE_URL}/${instrument}/candles${convertObjectToQuery(parameters)}`;
+  try {
+    const response = await axios.get(URL, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function getOrderBook(instrument, time = null) {
-    let url = `${BASE_URL}/${instrument}/orderBook`;
-    if (time != null) {
-        url += `?time=${time}`;
-    }
-    try {
-        const response = await axios.get(url, HEADER);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+  let url = `${BASE_URL}/${instrument}/orderBook`;
+  if (time != null) {
+    url += `?time=${time}`;
+  }
+  try {
+    const response = await axios.get(url, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 async function getPositionBook(instrument, time = null) {
-    let url = `${BASE_URL}/${instrument}/positionBook`;
-    if (time != null) {
-        url += `?time=${time}`;
-    }
-    try {
-        const response = await axios.get(url, HEADER);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-function convertObjectToQuery(object) {
-    const keys = Object.keys(object);
-    const values = Object.values(object);
-    const length = keys.length;
-
-    if (object != null) {
-        let query = '?';
-        for (let i = 0; i < length - 1; i += 1) {
-            query += `${keys[i]}=${values[i]}&`;
-        }
-        query += `${keys[length - 1]}=${values[length - 1]}`;
-        return query;
-    }
-    return ''
+  let url = `${BASE_URL}/${instrument}/positionBook`;
+  if (time != null) {
+    url += `?time=${time}`;
+  }
+  try {
+    const response = await axios.get(url, HEADER);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 module.exports = {
-    getCandlestickData,
-    getOrderBook,
-    getPositionBook
-}
+  getCandlestickData,
+  getOrderBook,
+  getPositionBook,
+};
